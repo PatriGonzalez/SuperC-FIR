25de418af4a285680d620eb48e5c62d1139314c9
PatriGonzalez <lg4er@virginia.edu>
2021-11-30 17:46:25 +0100

    Delete output directory
    Delete the output directory which is created at each iteration

--- output/stimulus.pl
+++
@@ -1,2 +0,0 @@
-my @coeffArray=('1100011', '1101101', '1111011', '1100000');
-my @xArray=(($clk_per*59)-$init_del-$clk_per/2, ($clk_per*40)-$init_del-$clk_per/2, ($clk_per*47)-$init_del-$clk_per/2, ($clk_per*120)-$init_del-$clk_per/2);
